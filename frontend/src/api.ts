1eae97f89cada1a320fe6c40ae593bfb62d0c199
servesh21 <serveshkhade68@gmail.com>
2025-08-08 23:03:41 +0530

    minor change

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://localhost:5000/api" || import.meta.env.BACKEND_URL;
 
 export const fetchAlerts = async () => {
   const response = await axios.get(`${BASE_URL}/alerts`);
