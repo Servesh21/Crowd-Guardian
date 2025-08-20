3011567d334b1146fb5d2dbe8defd0894739bc6f
Servesh Khade <161584037+Servesh21@users.noreply.github.com>
2025-08-20 19:55:36 +0530

    Update api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BACKEND_URL; || "http://localhost:5000/api" ;
+const BASE_URL = process.env.BACKEND_URL || "http://localhost:5000/api" ;
 
 export const fetchAlerts = async () => {
   const response = await axios.get(`${BASE_URL}/alerts`);
